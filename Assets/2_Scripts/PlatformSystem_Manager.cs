776e51ffcdc06cc9f2aaae0504327a655f391115
gino <wltn39@gmail.com>
2024-06-20 22:59:20 +0900

    플랫폼 배치 알고리즘

--- Assets/2_Scripts/PlatformSystem_Manager.cs
+++ Assets/2_Scripts/PlatformSystem_Manager.cs
@@ -10,7 +10,8 @@ public class PlatformSystem_Manager : MonoBehaviour
     [SerializeField] private Platform_Script[] middlePlatformClassArr = null;
     [SerializeField] private Platform_Script[] smallPlatformClassArr = null;
     private Dictionary<int, Platform_Script[]> platformClassArrDic;
-
+    private int platformNum;
+    [SerializeField] private Data[] dataArr = null;
     public void Init_Func()
     {
         this.platformClassArrDic = new Dictionary<int, Platform_Script[]>();
@@ -21,12 +22,22 @@ public class PlatformSystem_Manager : MonoBehaviour
     }
     public void Activate_Func()
     {
-        // LargePlatform
-        Platform_Script[] _platformClassArr = this.platformClassArrDic[2];
         Vector2 _pos = this.spawnPosTrf.position;
 
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 50; i++)
         {
+            int _platformID = -1;
+
+            foreach (Data _data in this.dataArr)
+            {
+                if (_data.TryGetPlatformID_Func(this.platformNum, out _platformID) == true)
+                {
+                    break;
+                }
+            }
+            // LargePlatform
+            Platform_Script[] _platformClassArr = this.platformClassArrDic[_platformID];
+
             int _randID = Random.Range(0, _platformClassArr.Length);
             Platform_Script _randPlatformClass = _platformClassArr[_randID];
 
@@ -34,34 +45,43 @@ public class PlatformSystem_Manager : MonoBehaviour
             _platformClass.Activate_Func(_pos);
 
             _pos += Vector2.right * 6f;
+
+            this.platformNum++;
         }
+    }
 
-        // middlePlatform
-        _platformClassArr = this.platformClassArrDic[1];
+    [System.Serializable]
+    public class Data
+    {
+        [SerializeField] private int conditionNum;
+        [SerializeField] private float[] percentArr = new float[3];
 
-        for (int i = 0; i < 5; i++)
+        public bool TryGetPlatformID_Func(int _platformNum, out int _platformID)
         {
-            int _randID = Random.Range(0, _platformClassArr.Length);
-            Platform_Script _randPlatformClass = _platformClassArr[_randID];
+            if (this.conditionNum <= _platformNum)
+            {
+                float _randValue = Random.value;
 
-            Platform_Script _platformClass = GameObject.Instantiate<Platform_Script>(_randPlatformClass);
-            _platformClass.Activate_Func(_pos);
-
-            _pos += Vector2.right * 6f;
-        }
-
-        // smallPlatform
-        _platformClassArr = this.platformClassArrDic[0];
-
-        for (int i = 0; i < 5; i++)
-        {
-            int _randID = Random.Range(0, _platformClassArr.Length);
-            Platform_Script _randPlatformClass = _platformClassArr[_randID];
-
-            Platform_Script _platformClass = GameObject.Instantiate<Platform_Script>(_randPlatformClass);
-            _platformClass.Activate_Func(_pos);
-
-            _pos += Vector2.right * 6f;
+                for (int i = 0; i < this.percentArr.Length; i++)
+                {
+                    if (_randValue < this.percentArr[i])
+                    {
+                        _platformID = i;
+                        return true;
+                    }
+                    else
+                    {
+                        _randValue -= this.percentArr[i];
+                    }
+                }
+                _platformID = 0;
+                return true;
+            }
+            else
+            {
+                _platformID = -1;
+                return false;
+            }
         }
     }
 }
